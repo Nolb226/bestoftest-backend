4a5aa60366ef1ab48dafe896502e170f0b656363
Nolb226 <hn26677@gmail.com>
2023-04-06 19:58:33 +0700

    test

--- util/database.js
+++ util/database.js
@@ -2,11 +2,12 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-	process.env.DB_NAME || 'qlttn',
-	process.env.DB_USERNAME || 'root',
-	process.env.DB_PASSWORD || '',
+	process.env.DB_NAME || 'bestoftest',
+	process.env.DB_USERNAME || 'qlttngroup5',
+	process.env.DB_PASSWORD || 'bestoftestgroup5',
 	{
-		host: 'localhost',
+		host: 'db4free.net',
+
 		dialect: 'mysql',
 		port: process.env.DB_PORT || '3306',
 	}
