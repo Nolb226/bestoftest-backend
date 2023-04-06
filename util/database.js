184afc689ca443d0f16768dbfa3ce332422fd817
Nolb226 <hn26677@gmail.com>
2023-04-06 22:33:21 +0700

    test

--- util/database.js
+++ util/database.js
@@ -2,14 +2,14 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-	process.env.DB_NAME || 'bestoftest',
-	process.env.DB_USERNAME || 'qlttngroup5',
-	process.env.DB_PASSWORD || 'bestoftestgroup5',
+	'bestoftest',
+	'qlttngroup5',
+	'bestoftestgroup5',
 	{
 		host: 'db4free.net',
 
 		dialect: 'mysql',
-		port: process.env.DB_PORT || '3306',
+		port: '3306',
 	}
 );
 module.exports = sequelize;
