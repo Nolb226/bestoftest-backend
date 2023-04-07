b1224e7b161d12abe12c6d4365c90c92c15979f0
Nolb226 <hn26677@gmail.com>
2023-04-07 14:43:01 +0700

    sdsd

--- util/database.js
+++ util/database.js
@@ -2,19 +2,19 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
-if (!process.env.NODE_ENV === 'test') {
-	sequelize = new Sequelize(
+if (process.env.NODE_ENV !== 'test') {
+	return (sequelize = new Sequelize(
 		'mysql://qs8bzb9tli4g3ip6:t562arn3ylweb754@grp6m5lz95d9exiz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/dy0jhdljqz4f0tqo',
 		{
 			dialect: 'mysql',
 		}
-	);
+	));
 } else {
-	sequelize = new Sequelize('hoang', 'root', '', {
+	return (sequelize = new Sequelize('hoang', 'root', '', {
 		host: 'localhost',
 		dialect: 'mysql',
 		port: '3307',
-	});
+	}));
 }
 // const sequelize = new Sequelize(
 // 	process.env.DB_NAME || 'bestoftest',
