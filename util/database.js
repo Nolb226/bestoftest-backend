d614d0d59f76093fc327f9ba4a05b9e5e3a96593
Nolb226 <hn26677@gmail.com>
2023-04-07 08:22:37 +0700

    s

--- util/database.js
+++ util/database.js
@@ -2,14 +2,20 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-	process.env.DB_NAME || 'bestoftest',
-	process.env.DB_USERNAME || 'qlttngroup5',
-	process.env.DB_PASSWORD || 'bestoftestgroup5',
+	'mysql://qs8bzb9tli4g3ip6:t562arn3ylweb754@grp6m5lz95d9exiz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/dy0jhdljqz4f0tqo',
 	{
-		host: process.env.DB_HOST || 'db4free.net',
-
 		dialect: 'mysql',
-		port: process.env.DB_PORT || '3306',
 	}
 );
+// const sequelize = new Sequelize(
+// 	process.env.DB_NAME || 'bestoftest',
+// 	process.env.DB_USERNAME || 'qlttngroup5',
+// 	process.env.DB_PASSWORD || 'bestoftestgroup5',
+// 	{
+// 		host: process.env.DB_HOST || 'db4free.net',
+
+// 		dialect: 'mysql',
+// 		port: process.env.DB_PORT || '3306',
+// 	}
+// );
 module.exports = sequelize;
