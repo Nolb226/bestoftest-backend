fcc26bb0a5942da6fa78593430cb9aa075a0898b
Nolb226 <hn26677@gmail.com>
2023-04-07 14:44:08 +0700

    sd

--- util/database.js
+++ util/database.js
@@ -3,18 +3,18 @@ require('dotenv').config();
 
 let sequelize;
 if (process.env.NODE_ENV !== 'test') {
-	return (sequelize = new Sequelize(
+	sequelize = new Sequelize(
 		'mysql://qs8bzb9tli4g3ip6:t562arn3ylweb754@grp6m5lz95d9exiz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/dy0jhdljqz4f0tqo',
 		{
 			dialect: 'mysql',
 		}
-	));
+	);
 } else {
-	return (sequelize = new Sequelize('hoang', 'root', '', {
+	sequelize = new Sequelize('hoang', 'root', '', {
 		host: 'localhost',
 		dialect: 'mysql',
 		port: '3307',
-	}));
+	});
 }
 // const sequelize = new Sequelize(
 // 	process.env.DB_NAME || 'bestoftest',
